506aecb8f6a645df0671badc8d1a59c1f14d7131
Trickshotblaster <brodiehoener@icloud.com>
2024-09-02 18:36:01 -0700

    skip video functionality, QOL improvements

--- env/index.py
+++ env/index.py
@@ -66,8 +66,9 @@ def get_random_youtube_video(api_key, language='es', max_results=50):
         ).execute()
 
         caption_tracks = video_response['items'][0]['contentDetails'].get('caption', 'false')
-
-        if caption_tracks == 'true':
+        yt = YouTube(f'https://www.youtube.com/watch?v={random_video_id}')
+        has_language = language in yt.captions or 'a.' + language in yt.captions
+        if caption_tracks == 'true' and has_language:
             return f'https://www.youtube.com/watch?v={random_video_id}'
         else:
             # If no captions, try again (recursively)
@@ -96,30 +97,37 @@ def filter_example(example):
     return True
 
 def get_caption_sound_pairs(video_url, lang='a.es'):
-    yt = YouTube(video_url)
-    caption = yt.captions[lang] if lang in yt.captions else yt.captions["a." + lang]
-    captions_dict = xmltodict.parse(caption.xml_captions)
-    examples = captions_dict['transcript']['text']
-    yt.streams.filter(only_audio=True)[0].download(output_path="audio/", filename="tmp")
-    video_sound = AudioSegment.from_file("audio/tmp")
+    try:
+        yt = YouTube(video_url)
+        caption = yt.captions[lang] if lang in yt.captions else yt.captions["a." + lang]
+        captions_dict = xmltodict.parse(caption.xml_captions)
+        examples = captions_dict['transcript']['text']
+        yt.streams.filter(only_audio=True)[0].download(output_path="audio/", filename="tmp")
+        video_sound = AudioSegment.from_file("audio/tmp")
 
-    captions, sounds = [], []
-    for index, example in enumerate(examples):
-        if filter_example(example):
-            start = int(float(example['@start']) * 1000)
-            dur = int(float(example['@dur']) * 1000)
-            if index < len(examples) - 1:
-                end = int(float(examples[index+1]['@start']) * 1000) + 100 # add a little extra time
-            else:
-                end = start + dur
-            if index > 0 and int(float(examples[index-1]['@dur']) * 1000) > 200:
-                start -= 200
-            capt = example['#text']
-            sound = video_sound[start:end]
-            
-            captions.append(capt)
-            sounds.append(sound)
-    return captions, sounds
+        captions, sounds = [], []
+        for index, example in enumerate(examples):
+            if filter_example(example):
+                start = int(float(example['@start']) * 1000)
+                dur = int(float(example['@dur']) * 1000)
+                if index < len(examples) - 1:
+                    end = int(float(examples[index+1]['@start']) * 1000) + 100 # add a little extra time
+                else:
+                    end = start + dur
+                if index > 0 and int(float(examples[index-1]['@dur']) * 1000) > 200:
+                    start -= 200
+                capt = example['#text']
+                sound = video_sound[start:end]
+                
+                captions.append(capt)
+                sounds.append(sound)
+        
+        return captions, sounds
+    except Exception as e:
+        print("find video failed with error:", e)
+        video_url = get_random_youtube_video(api_key, lang)
+        return get_caption_sound_pairs(video_url, lang)
+
 
 def jaccard_similarity(x,y):
   """ returns the jaccard similarity between two lists """
@@ -127,9 +135,21 @@ def jaccard_similarity(x,y):
   union_cardinality = len(set.union(*[set(x), set(y)]))
   return intersection_cardinality/float(union_cardinality)
 
+def clean_string(string):
+    punctuation = ".,:?¿!¡'[];"
+    for p in punctuation:
+        string = string.replace(p, "")
+    string = unidecode(string).lower()
+    return string
+
 lang = 'es'
 random_video_url = get_random_youtube_video(api_key, language=lang)
 captions, sounds = get_caption_sound_pairs(random_video_url, lang=lang)
+c = list(zip(captions, sounds))
+
+random.shuffle(c)
+
+captions, sounds = zip(*c)
 
 current_index = 0
 
@@ -137,9 +157,20 @@ current_index = 0
 @app.route("/", methods=['GET', 'POST'])
 def hello_world():
     global current_index
+    global captions
+    global sounds
+    if current_index >= len(captions):
+        random_video_url = get_random_youtube_video(api_key, language=lang)
+        captions, sounds = get_caption_sound_pairs(random_video_url, lang=lang)
+        c = list(zip(captions, sounds))
+
+        random.shuffle(c)
+
+        captions, sounds = zip(*c)
     play_sound = request.form.get("play")
     go_next = request.form.get("go_next")
     guess = request.form.get("guess")
+    skip_video = request.form.get("skip_video")
     if play_sound:
         play(sounds[current_index])
     if go_next:
@@ -148,13 +179,21 @@ def hello_world():
     if guess:
         print("you guessed:", guess)
         print("correct answer:", unidecode(captions[current_index]).lower())
-        similarity = jaccard_similarity(unidecode(guess).lower(), unidecode(captions[current_index]).lower())
+        similarity = jaccard_similarity(clean_string(guess), clean_string(captions[current_index]))
         print("similarity:", similarity)
         correct = similarity > 0.8
         msg = "---Correct---" if correct else "---Incorrect---"
         print(msg)
         socketio.emit('server', msg + f"\nAnswer: {captions[current_index]}")
         current_index += 1
+    if skip_video:
+        random_video_url = get_random_youtube_video(api_key, language=lang)
+        captions, sounds = get_caption_sound_pairs(random_video_url, lang=lang)
+        c = list(zip(captions, sounds))
+
+        random.shuffle(c)
+
+        captions, sounds = zip(*c)
     return render_template('index.html')
 
 if __name__ == '__main__':
